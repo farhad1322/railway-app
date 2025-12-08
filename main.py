b752a5fb6516ea0ff36318b4cdd65431b1d34e4b
farhad1322 <farhadabass2018@gmail.com>
2025-12-09 00:09:31 +0300

    l2

--- main.py
+++ main.py
@@ -5,13 +5,3 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"status": "Railway is running ✅"}
-
-
-
-fastapi==0.110.0
-uvicorn==0.29.0
-
-
-
-
-web: uvicorn main:app --host 0.0.0.0 --port $PORT
