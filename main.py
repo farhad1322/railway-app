0d08477202acd512b74c102ecd20e3fd11735e8d
farhad1322 <farhadabass2018@gmail.com>
2025-12-08 22:04:51 +0300

    third update
    Updated the home endpoint response to include a checkmark emoji.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPIimport os
+from fastapi import FastAPI
+import os
 app = FastAPI()
-@app.get("/")def home():    return {"status": "Railway is running "}
-if __name__ == "__main__":    import uvicorn    uvicorn.run(        "main:app",        host="0.0.0.0",        port=int(os.environ.get("PORT", 3000))    )
-
-
+@app.get("/")
+def home():
+  return {"status": "Railway is running ✅"}
+if __name__ == "__main__":
+  import uvicorn
+  uvicorn.run(
+    "main:app",
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT",
+                            3000))
+  )
