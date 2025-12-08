55f243b670947d45a327162af1b6dd2ab6316882
farhad1322 <farhadabass2018@gmail.com>
2025-12-08 22:48:12 +0300

    fix4

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+import os
+
 app = FastAPI()
+
 @app.get("/")
 def home():
-    return {"status": "Railway is running
-    ✅"}
+    return {"status": "Railway is running "}
+    
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 3000))
+    )
