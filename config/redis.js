384d986c148cbafc474dcd231332aa35cf50eb41
farhad1322 <farhadabass2018@gmail.com>
2025-12-19 00:05:46 +0300

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -1,16 +1,13 @@
 const Redis = require("ioredis");
 
-const redis = new Redis(process.env.REDIS_URL, {
-  maxRetriesPerRequest: null,
-  enableReadyCheck: false,
-});
+const redis = new Redis(process.env.REDIS_URL);
 
 redis.on("connect", () => {
   console.log("✅ Redis connected");
 });
 
 redis.on("error", (err) => {
-  console.error("❌ Redis error:", err.message);
+  console.error("❌ Redis error", err);
 });
 
 module.exports = redis;
